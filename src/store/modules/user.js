eadd467d1afe924a8d00ccd941b67f5ddbc40e09
jason <jason.zhao86@outlook.com>
2023-02-04 11:53:55 +0800

    完成login页面的开发

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,7 +1,42 @@
+import { getToken, setToken, removeToken } from "@/utils/auth";
+import { login } from "@/api/user";
+
+const state = {
+  token: getToken(),
+};
+
+const mutations = {
+  setToken(state, token) {
+    state.token = token;
+    setToken(token);
+  },
+  removeToken(state) {
+    state.token = null;
+    removeToken();
+  },
+};
+
+const actions = {
+  // async login(context, data) {
+  //   const res = await login(data);
+  //   context.commit("setToken", res);
+  // }
+  login(context, data) {
+    return new Promise((resolve) => {
+      login(data).then((res) => {
+        context.commit("setToken", res);
+        resolve();
+      });
+    });
+  },
+};
+
+const getters = {};
+
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {},
-  getters: {},
+  state,
+  mutations,
+  actions,
+  getters,
 };
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,27 @@
 import axios from "axios";
+import { Message } from "element-ui";
 
-const service = axios.create({});
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000,
+});
+
 service.interceptors.request.use();
-service.interceptors.response.use();
+
+service.interceptors.response.use(
+  (response) => {
+    const { success, data, message } = response.data;
+    if (success) {
+      return data;
+    } else {
+      Message.error(message);
+      return Promise.reject(new Error(message));
+    }
+  },
+  (error) => {
+    Message.error(error.message);
+    return Promise.reject(error);
+  }
+);
 
 export default service;
